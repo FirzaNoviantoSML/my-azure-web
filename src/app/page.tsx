24059f1c5e7782b9990df32bd1a6f2bc1beb7ff0
firzaNovianto <virza56@gmail.com>
2025-01-23 15:35:17 +0700

    btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -6,6 +6,11 @@ export default function Home() {
   return (
     <div>
      Hellowwww worrrrlllllddd!!!
+     <Link href="/hello">
+     <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-950 transition">
+      Hallo
+     </button>
+     </Link>
 
     </div>
   );
