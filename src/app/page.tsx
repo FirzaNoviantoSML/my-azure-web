6268bb173c4ff6f4c757976adef0932120d68e00
firzaNovianto <virza56@gmail.com>
2025-01-23 15:42:29 +0700

    numb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Link from "next/link";
+import {useState } from "react";
 
 export default function Home() {
 
+const [numb,setNumb] = useState(0)
+
+const numbHandler = () => {
+  setNumb(numb +1)
+}
   console.log("App is starting...");
   return (
     <div>
@@ -11,7 +19,10 @@ export default function Home() {
       Hallo
      </button>
      </Link>
-
+     <button onClick={() => numbHandler()} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-950 transition">
+      tambah
+     </button>
+      {numb}
     </div>
   );
 }
